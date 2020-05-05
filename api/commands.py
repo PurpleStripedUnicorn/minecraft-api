387bd906c2088ef9d6f8eeab0905bb02b77122fa
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2020-05-05 15:00:20 +0200

    Added 'if' function

--- api/commands.py
+++ api/commands.py
@@ -1,5 +1,6 @@
 
 import os
+import random
 
 from api.command import Command, CommandContainer
 from api.context import Context
@@ -13,12 +14,27 @@ def cmdNamespace (cp, line):
     ''' namespace definition line is ignored when compiling, since it has
     already been searched for, namespace definitions have to be inside the main 
     context '''
-    print(len(cp.contextStack))
     if len(cp.contextStack) != 1:
         raise cp.raiseError('namespace definition outside main context')
 
 @commands('def', r'[A-Za-z0-9_]+ *\{ *$')
 def cmdDef (cp, line):
+    ''' function definition '''
     fname = removeWhitespace(line.split(' ')[1].split('{')[0])
+    cp.contextStack.append(Context(os.path.join(cp.mainfolder, 'functions',
+    fname + '.mcfunction')))
+
+@commands('if', r'^.+\{ *$')
+def cmdIf (cp, line):
+    ''' `if` command, is a shorthand for using a function and the vanilla
+    `execute` command '''
+    # remove whitespace and '{' from the statement
+    statement = removeWhitespace(removeWhitespace(line)[:-1])
+    # generate a random function name
+    fname = '0x{:08x}'.format(random.randint(0, 16 ** 8 - 1))
+    with open(cp.currentOutputfile, 'a') as f:
+        f.write('execute {} run function {}:{}'.format(statement, cp.namespace,
+        fname))
+    # create new context for the function file
     cp.contextStack.append(Context(os.path.join(cp.mainfolder, 'functions',
     fname + '.mcfunction')))
